29f50f10a9a64e15876b233577c0d601edfa449a
Nadia-Kadri <nadiagrace96@gmail.com>
2019-12-27 15:01:26 -0500

    api and html routing

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+let friendsArray = [
+  {
+    name: "Conrad",
+    photo: "../images/friends/conrad.jpg",
+    scores: [5, 2, 4, 3, 3, 1, 4, 2, 2, 5]
+  },
+  {
+    name: "Nic",
+    photo: "../images/friends/nic.jpg",
+    scores: [4, 4, 5, 3, 2, 1, 1, 2, 5, 4]
+  },
+  {
+    name: "Devon",
+    photo: "../images/friends/devon.jpg",
+    scores: [2, 4, 2, 3, 2, 3, 3, 4, 5, 3]
+  },
+  {
+    name: "Mina",
+    photo: "../images/friends/mina.jpg",
+    scores: [5, 5, 1, 4, 2, 5, 5, 5, 2, 1]
+  },
+  {
+    name: "Randolph",
+    photo: "../images/friends/randolph.jpg",
+    scores: [2, 3, 4, 5, 3, 3, 3, 5, 3, 3]
+  },
+  {
+    name: "AJ",
+    photo: "../images/friends/aj.jpg",
+    scores: [4, 4, 4, 5, 5, 3, 5, 5, 2, 3]
+  }
+]
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,40 @@
+var friendsData = require("../data/friends");
+
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    let checking = 0;
+    let bestMatch;
+    let score = 100;
+
+    userData = req.body;
+    console.log(userData);
+    userData.scores = userData.scores.map(function(item) {
+      return parseInt(item, 10);
+    });
+
+    sum = userData.scores.reduce((a, b) => a + b, 0);
+
+    for (let i = 0 ; i < friendsData.length ; i++) {
+      let friendScore = friendsData[i].scores.reduce((a, b) => a + b, 0);
+      let totalDifference = Math.abs(sum - friendScore);
+
+      if (totalDifference <= score) {
+        bestMatch = friendsData[i];
+        score = totalDifference;
+      }  
+
+      checking++
+      // console.log(checking)
+    };
+
+    if (friendsData.length = checking) {
+        friendsData.push(userData);
+        // console.log(friendsData)
+        res.json(bestMatch)
+    }
+  });
+};
